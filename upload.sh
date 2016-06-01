af9c28ef424bd2a62f87d6b62b28efa3f5a4a63a
Sébastien Aupetit <sebtic@projectsforge.org>
2016-06-01 09:47:17 +0200

    Improve CI

--- upload.sh
+++ upload.sh
@@ -8,5 +8,5 @@ PROJECT_PATH=$(echo ${CI_BUILD_REPO} | sed 's~http[s]*://[^/]*\(.*\).git$~\1~')
 FILES_VERSION=$(grep '\\def\\polytechfileversion' polytech/polytech.cls | sed 's~\\def\\polytechfileversion{\(.*\)}~\1~' )
 FILES_DATE=$(grep '\\def\\polytechfiledate' polytech/polytech.cls | sed 's~\\def\\polytechfiledate{\(.*\)}~\1~' | sed 's~/~.~g')
 
-curl -v -u $NEXUS_USER:$NEXUS_PASSWORD --upload-file polytech.zip $PROJECT_PATH/$CI_BUILD_REF_NAME/polytech-$FILES_VERSION-${FILES_DATE/\//.}.zip
+echo curl -v -u $NEXUS_USER:$NEXUS_PASSWORD --upload-file polytech.zip $PROJECT_PATH/$CI_BUILD_REF_NAME/polytech-$FILES_VERSION-${FILES_DATE/\//.}.zip
 
