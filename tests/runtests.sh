4081f258b43607329fbc9366de3a5e28923a70b9
Sébastien Aupetit <sebtic@projectsforge.org>
2016-06-01 08:53:13 +0200

    Add missing \addbibresource{biblio} in tests

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -13,6 +13,7 @@ find . -maxdepth 1 -mindepth 1 -type d | sort | while read TESTDIR; do
     cd "$SCRIPTPATH/$TESTDIR"
     ln -sf ../../polytech
     rm -f test-latexmk.log
+    find . -iname "*.bbl" -delete
     latexmk -gg -pdf -interaction=nonstopmode test.tex >> test-latexmk.log 2>> test-latexmk.log
     result=$?
     
